aeaf800f844b7c35c90d053c0c9fe44b3ff4fff9
tuck3rb <brocktk@hendrix.edu>
2024-09-21 16:11:56 -0500

    We've got a navbar!
    I also readded some things I didn't mean to remove. https://api.flutter.dev/flutter/material/NavigationBar-class.html

--- big_mics_playtime/lib/main.dart
+++ big_mics_playtime/lib/main.dart
@@ -15,9 +15,47 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         useMaterial3: true,
       ),
-      // home: const MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Colors.amber,
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.play_arrow),
+            icon: Icon(Icons.play_arrow),
+            label: 'Play',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.mic),
+            icon: Icon(Icons.mic),
+            label: 'Test',
+          ),
+        ],
+      ),
+    );
+  }
+
+}
