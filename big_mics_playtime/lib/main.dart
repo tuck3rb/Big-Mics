13c271105745c2b2356c036ef34137b11237748c
tuck3rb <brocktk@hendrix.edu>
2024-09-21 16:33:35 -0500

    Majority of the skeleton and nav set up
    Need to do some more of the big chunks now

--- big_mics_playtime/lib/main.dart
+++ big_mics_playtime/lib/main.dart
@@ -34,6 +34,44 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
     return Scaffold(
+      body: <Widget>[
+        // Play page
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 60),
+              const Text(
+                'Make noise to jump.',
+                style: TextStyle(fontSize: 25),
+              ),
+              const SizedBox(height: 30), // Functions as a spacer
+              Container( 
+                width: 350,
+                height: 350,
+                child: Placeholder(), // Future implementation of game
+              ),
+              const SizedBox(height: 30), // Functions as a spacer
+              const Text(
+                'Score: ', // Will need to update this to be non const and add in score variable
+                style: TextStyle(fontSize: 25),
+              ),
+              const SizedBox(height: 30), // Functions as a spacer
+              const Text(
+                'High Score: ', // Will need to update this to be non const and add in high_score variable
+                style: TextStyle(fontSize: 25),
+              ),
+            ],
+          ),
+        ),
+        // Mic Test page
+        const Center(
+          child: Text(
+            'Test microphone.',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+    ][currentPageIndex],
       bottomNavigationBar: NavigationBar(
         onDestinationSelected: (int index) {
           setState(() {
