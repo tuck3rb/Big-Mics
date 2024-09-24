e5dd44cc58c3f057ad69fc872ca62aebe6c6d3ac
tuck3rb <brocktk@hendrix.edu>
2024-09-24 10:52:55 -0500

    Portrait only

--- big_mics_playtime/lib/main.dart
+++ big_mics_playtime/lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized(); // Forces portrait mode
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const MyApp());
 }
 
