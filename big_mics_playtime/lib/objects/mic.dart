bfabdfd98a29780fe25ee2277486a4ff260b4622
JGrisby2 <Jayden.Grisby@gmail.com>
2024-10-22 12:58:25 -0500

    Added comments to mic.dart

--- big_mics_playtime/lib/objects/mic.dart
+++ big_mics_playtime/lib/objects/mic.dart
@@ -4,22 +4,27 @@ import 'dart:async';
 class Mic {
   NoiseReading? _latestReading;
   StreamSubscription<NoiseReading>? _noiseSubscription;
-  double maxVol = 0.0;
-  double minVol = 50.0;
+  double maxVol = 0.0; //The highest recorded mic reading
+  double minVol = 60.0; //The value required for Bic Mic to jump
   bool recording = false;
   NoiseMeter noiseMeter = NoiseMeter();
 
+  //Starts the mic, updating _latestReading and maxVol until stop is
+  //called
   Future<void> start() async {
     _latestReading = null;
     maxVol = 0;
     _noiseSubscription = noiseMeter.noise.listen(onData);
 
   }
-
+  //Stops the mic and stopls _latestReading and maxVol from updating.
   void stop() {
     _noiseSubscription?.cancel();
   }
 
+  //The method that the noiseMeter listens to when start is called
+  //Sets the maxVol as well as the _latestReading
+  //Use _latestReading!.meanDecibel if you want _latestReading as a double.
   void onData(NoiseReading noiseReading) {
     _latestReading = noiseReading;
     if (_latestReading != null) {
